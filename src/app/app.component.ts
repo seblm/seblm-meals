869055221b83cb39108256870ffd02c249895821
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2019-12-06 22:58:13 +0100

    improve ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'seblm-meals';
+  start = moment().locale('fr').startOf('week');
+  end = moment().locale('fr').endOf('week');
+  current(): string {
+    console.log(this.currentMenus);
+    if (this.start.month() == this.end.month()) {
+      return `Du ${this.start.format('D')} au ${this.end.format('D MMMM YYYY')} `
+    } else {
+      return `Du ${this.start.format('D MMMM')} au ${this.end.format('D MMMM YYYY')} `
+    }
+  }
+  currentMenus: Menu[] = [
+      new Menu('lundi', 'galettes de légumes riz'),
+      new Menu('mardi', 'chou-fleur pomme de terre lardons'),
+      new Menu('mercredi midi', 'chou-fleur pomme de terre lardons'),
+      new Menu('mercredi soir', 'chou-fleur pomme de terre lardons'),
+      new Menu('jeudi', 'chou-fleur pomme de terre lardons'),
+      new Menu('vendredi', 'chou-fleur pomme de terre lardons'),
+      new Menu('samedi midi', 'chou-fleur pomme de terre lardons'),
+      new Menu('samedi soir', 'chou-fleur pomme de terre lardons'),
+      new Menu('dimanche midi', 'chou-fleur pomme de terre lardons'),
+      new Menu('dimanche soir', 'chou-fleur pomme de terre lardons'),
+    ]
+}
+
+export class Menu {
+  constructor(public day: string, public meal: string) { }
 }
