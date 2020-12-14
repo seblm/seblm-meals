d71e26219f32cd22dabc6eb9712a0d610fb937f1
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2020-12-14 01:15:49 +0100

    :rotating_light: avoid using an implicit conversion

--- test/meals/MealsServiceSpec.scala
+++ test/meals/MealsServiceSpec.scala
@@ -1,8 +1,5 @@
 package meals
 
-import java.time._
-import java.util.UUID
-
 import meals.domain._
 import meals.infrastructure.MealRow
 import org.mockito.scalatest.IdiomaticMockito
@@ -11,6 +8,8 @@ import org.scalatest.concurrent.ScalaFutures.whenReady
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 
+import java.time._
+import java.util.UUID
 import scala.concurrent.ExecutionContext.global
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -85,14 +84,16 @@ class MealsServiceSpec extends AnyFlatSpec with IdiomaticMockito {
   it should "randomly choose a meal" in withRepositoryAndService {
     (mealRepository: MealRepository, mealsService: MealsService) =>
       val day = LocalDateTime.parse("2020-03-02T20:00")
-      mealRepository.all() returns MealsServiceSpec.AllResponse(
-        Seq(
-          "monday next week                          ->                   2020-03-02T20:00",
-          "monday current week and tuesday next week -> 2020-02-24T20:00, 2020-03-03T20:00",
-          "tuesday current week                      -> 2020-02-25T20:00",
-          "wednesday current week                    -> 2020-02-26T20:00"
+      mealRepository.all() returns MealsServiceSpec
+        .AllResponse(
+          Seq(
+            "monday next week                          ->                   2020-03-02T20:00",
+            "monday current week and tuesday next week -> 2020-02-24T20:00, 2020-03-03T20:00",
+            "tuesday current week                      -> 2020-02-25T20:00",
+            "wednesday current week                    -> 2020-02-26T20:00"
+          )
         )
-      )
+        .toFuture
       mealRepository.link(*, eqTo(day)) answers { (mealRow: MealRow, _: LocalDateTime) =>
         Future.successful(Meal(day, mealRow.description))
       }
@@ -104,18 +105,20 @@ class MealsServiceSpec extends AnyFlatSpec with IdiomaticMockito {
 
   it should "randomly choose all empty meals for a week" in withRepositoryAndService {
     (mealRepository: MealRepository, mealsService: MealsService) =>
-      mealRepository.all() returns MealsServiceSpec.AllResponse(
-        Seq(
-          "monday        -> 2020-02-17T20:00",
-          "tuesday       -> 2020-02-18T20:00",
-          "wednesday     -> 2020-02-19T20:00",
-          "thursday      -> 2020-02-20T20:00",
-          "friday        -> 2020-02-21T20:00",
-          "saturday      -> 2020-02-22T20:00",
-          "sunday        -> 2020-02-23T20:00",
-          "previous week -> 2020-02-24T20:00, 2020-02-25T20:00, 2020-02-26T20:00, 2020-02-27T20:00, 2020-02-28T20:00, 2020-02-29T20:00, 2020-03-01T20:00"
+      mealRepository.all() returns MealsServiceSpec
+        .AllResponse(
+          Seq(
+            "monday        -> 2020-02-17T20:00",
+            "tuesday       -> 2020-02-18T20:00",
+            "wednesday     -> 2020-02-19T20:00",
+            "thursday      -> 2020-02-20T20:00",
+            "friday        -> 2020-02-21T20:00",
+            "saturday      -> 2020-02-22T20:00",
+            "sunday        -> 2020-02-23T20:00",
+            "previous week -> 2020-02-24T20:00, 2020-02-25T20:00, 2020-02-26T20:00, 2020-02-27T20:00, 2020-02-28T20:00, 2020-02-29T20:00, 2020-03-01T20:00"
+          )
         )
-      )
+        .toFuture
       mealRepository.link(*, *) answers { (mealRow: MealRow, time: LocalDateTime) =>
         Future.successful(Meal(time, mealRow.description))
       }
@@ -145,21 +148,25 @@ object MealsServiceSpec {
 
   case class AllResponse(all: Seq[String])
 
-  private implicit def toAllResponse(allResponse: AllResponse): Future[Map[MealRow, Seq[LocalDateTime]]] =
-    Future.successful(toAllResponseMap(allResponse))
+  implicit class AllResponseOps(allResponse: AllResponse) {
 
-  private def toAllResponseMap(allResponse: AllResponse): Map[MealRow, Seq[LocalDateTime]] = allResponse.all match {
-    case Nil          => Map.empty
-    case last :: Nil  => Map(toAllResponseMap(last))
-    case head :: tail => Map(toAllResponseMap(head)) ++ toAllResponseMap(AllResponse(tail))
-  }
+    def toFuture: Future[Map[MealRow, Seq[LocalDateTime]]] =
+      Future.successful(toAllResponseMap(allResponse))
 
-  private def toAllResponseMap(line: String): (MealRow, Seq[LocalDateTime]) = line.split("->") match {
-    case Array(description, dates) =>
-      MealRow(UUID.randomUUID(), description.trim()) -> dates
-        .split(",")
-        .toIndexedSeq
-        .map(x => LocalDateTime.parse(x.trim))
+    private def toAllResponseMap(allResponse: AllResponse): Map[MealRow, Seq[LocalDateTime]] = allResponse.all match {
+      case Nil          => Map.empty
+      case last :: Nil  => Map(toAllResponseMap(last))
+      case head :: tail => Map(toAllResponseMap(head)) ++ toAllResponseMap(AllResponse(tail))
+    }
+
+    private def toAllResponseMap(line: String): (MealRow, Seq[LocalDateTime]) = line.split("->") match {
+      case Array(description, dates) =>
+        MealRow(UUID.randomUUID(), description.trim()) -> dates
+          .split(",")
+          .toIndexedSeq
+          .map(x => LocalDateTime.parse(x.trim))
+    }
+
   }
 
 }
