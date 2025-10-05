7757403cbee3b179b36f9018e803e6c7c188ca6d
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2025-10-05 08:57:59 +0200

    :sparkles: print terms ordered by frequency

--- catseffect/src/test/scala/meals/stats/StatsSuite.scala
+++ catseffect/src/test/scala/meals/stats/StatsSuite.scala
@@ -2,12 +2,13 @@ package meals.stats
 
 import cats.effect.{IO, Resource}
 import munit.CatsEffectSuite
+import natchez.Trace.Implicits.noop
 import org.slf4j.{Logger, LoggerFactory}
 import skunk.Session
-import skunk.implicits.sql
 import skunk.codec.text.text
-import natchez.Trace.Implicits.noop
+import skunk.implicits.sql
 
+import scala.collection.immutable.{SortedMap, SortedSet}
 import scala.concurrent.duration.*
 
 class StatsSuite extends CatsEffectSuite:
@@ -19,20 +20,39 @@ class StatsSuite extends CatsEffectSuite:
     val host = Option(System.getenv("POSTGRESQL_ADDON_HOST")).getOrElse("localhost")
     val db = Option(System.getenv("POSTGRESQL_ADDON_DB")).getOrElse("seblm-meals")
     val session: Resource[IO, Session[IO]] =
-      Session.single(
-        host = host,
-        user = user,
-        database = db,
-        password = Some(password),
-      )
+      Session.single(host = host, user = user, database = db, password = Some(password))
+    val excluded = List("de", "aux", "au", "à", "a", "la", "et", "l", "d", "en")
     val result: IO[List[String]] = session.use: session =>
       for
+        descriptions: List[String] <- session.execute[String](sql"SELECT description FROM meals".query(text))
+        _ <- IO.println(s"${descriptions.length} descriptions")
+        terms = descriptions
+          .map(description => description.replaceAll("[',:&()/]", " "))
+          .flatMap(description => description.split("\\s+"))
+          .filterNot(term => excluded.contains(term))
+        _ <- IO.println(s"${terms.length} terms")
+        _ <- IO.println(s"${terms.distinct.length} distinct terms")
+        reversedIntOrdering = implicitly[Ordering[Int]].reverse
+        byOccurrence = terms
+          .groupBy(identity)
+          .view
+          .mapValues(_.length)
+          .toMap
+          .foldLeft(SortedMap[Int, SortedSet[String]]()(reversedIntOrdering)): (map, termAndCount) =>
+            map.updatedWith(termAndCount._2):
+              case Some(terms) => Some(terms.incl(termAndCount._1))
+              case None        => Some(SortedSet(termAndCount._1))
+        byOccurrenceString = byOccurrence
+          .map: (count, terms) =>
+            terms.map(term => s"$term ($count)").mkString("\n")
+          .mkString("\n")
+        _ <- IO.println(s"ordered by most frequent:\n$byOccurrenceString")
         meals <- session.execute[String](
-            sql"""SELECT meals.description FROM meals
-                 |INNER JOIN meals_by_time ON meals.id = meals_by_time.meal_id
-                 |WHERE to_char(meals_by_time.time, 'day HH24:MI') = 'monday    20:00'
-                 |LIMIT 10""".stripMargin.query(text)
-          )
+          sql"""SELECT meals.description FROM meals
+               |INNER JOIN meals_by_time ON meals.id = meals_by_time.meal_id
+               |WHERE to_char(meals_by_time.time, 'day HH24:MI') = 'monday    20:00'
+               |LIMIT 10""".stripMargin.query(text)
+        )
       yield meals
 
     assertIO(
