459ba9d56b395a3b0d96c06a2579208194ddca68
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2026-01-05 22:58:18 +0100

    :alembic: mobile version

---
+++ frontend/src/routes/mobile/+page.svelte
@@ -0,0 +1,21 @@
+<!-- https://matthewjamestaylor.com/2-column-layouts -->
+<div class="day">
+	<div class="meal-lunch">lunch</div>
+	<div class="meal-dinner">dinner</div>
+</div>
+
+<style>
+	.day {
+		display: flex;
+	}
+	.day > * {
+		width: 50%;
+		padding: 1rem;
+	}
+	.meal-lunch {
+		background-color: lightgrey;
+	}
+	.meal-dinner {
+		background-color: darkgrey;
+	}
+</style>
