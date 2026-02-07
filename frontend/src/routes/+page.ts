a8e116b2b415126e34fe05221851d99c28ca90ec
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2026-02-07 17:13:41 +0100

    :art: fix formatting with Prettier

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -5,8 +5,10 @@ export const load = async () => {
 	if (dev) {
 		const currentDate = new Date();
 		const januaryFirst = new Date(currentDate.getFullYear(), 0, 1);
-		const numberOfDays = Math.floor((currentDate.valueOf() - januaryFirst.valueOf()) / (24 * 60 * 60 * 1000));
+		const numberOfDays = Math.floor(
+			(currentDate.valueOf() - januaryFirst.valueOf()) / (24 * 60 * 60 * 1000)
+		);
 		const weekNumber = Math.ceil((currentDate.getDay() + 1 + numberOfDays) / 7);
 		redirect(307, `/meals/${currentDate.getFullYear()}/${weekNumber}`);
 	}
-}
+};
