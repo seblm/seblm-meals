b078731c27a354cc63c1fe7108bb1449ee800ee3
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2026-09-12 11:11:05 +0200

    :truck: prepare to move away from picocss

---
+++ frontend/src/routes/meals/statistics/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2.1.1/css/pico.min.css" />
+</svelte:head>
+
+{@render children?.()}
